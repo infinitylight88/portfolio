ee4095e62b3a0e4e8aa2c8ef6c03155819cba12a
Derrick Napokoli <napokoliderrick88@gmail.com>
2024-07-03 22:53:00 +0300

    Update Expereance.jsx
    Using   .map() method to display data dynamically.

--- src/Components/Expereance/Expereance.jsx
+++ src/Components/Expereance/Expereance.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import skills from "../../data/skills.json"
+import { getImageUrl } from '../../utils'
 
 const Expereance = () => {
   return (
     <section id='expereance'>
         <h2>Expereance</h2>
         <div>
-            <div></div>
+            <div>
+                {
+                    skills.map((skill, id) => {
+                        return <div key={idd}>
+                            <img src={skill.getImageUrl(skill.imggeSrc)} alt={skill.title} />
+                             </div>
+                    })
+                }
+            </div>
             <ul></ul>
         </div>
 
