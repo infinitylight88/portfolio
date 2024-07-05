ab40481aff2008d54bec50775c32eb2a3ee2ab46
Derrick Napokoli <napokoliderrick88@gmail.com>
2024-07-05 22:44:31 +0300

    completed html for Experience component
    Completed Dynamic rendering for history component.

--- src/Components/Expereance/Expereance.jsx
+++ src/Components/Expereance/Expereance.jsx
@@ -1,52 +1,56 @@
-import React from 'react'
-import skills from "../../data/skills.json"
-import history from "../../data/history.json"
-import { getImageUrl } from '../../utils'
+import React from 'react';
+import skills from "../../data/skills.json";
+import history from "../../data/history.json";
+import { getImageUrl } from '../../utils';
+import styles from './Expereance.module.css';
 
 const Expereance = () => {
-  return (
-    <section id='expereance'>
-        <h2>Expereance</h2>
-        <div>
-            <div>
-                {
-                    skills.map((skill, id) => {
-                        return <div key={id}>
-                            <div><img src={skill.getImageUrl(skill.imggeSrc)} alt={skill.title} /></div>
-                            <p>{skills.title}</p>
-                             </div>
-                    })
-                }
-            </div>
-            <ul>
-                {history.map((historyItem, id ) => {
-
-                    return(
-                        <li key={id}>
-                            <img src= {getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} logo`} />
-                            <div>
-                                <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
-                                <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                                <ul>
-                                    {historyItem.expereances.map((expereance, id) =>{
-                                        return(
-                                            <li key={id}>
-                                                {expereance}
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
+    return (
+        <section className={styles.container} id='expereance'>
+            <h2 className={styles.title}>Expereance</h2>
+            <div className={styles.content}>
+                <div style={styles.skills}>
+                    {
+                        skills.map((skill, id) => {
+                            return <div key={id} className={styles.skill}>
+                                <div className={styles.skillImageContainer}>
+                                    <img src={getImageUrl(skill.imgeSrc)} alt={skill.title} />
+                                </div>
+                                <p>{skill.title}</p>
                             </div>
+                        })
+                    }
+                </div>
+                <ul className={styles.history}>
+                    {history.map((historyItem, id) => {
 
-                        </li>
-                    )
+                        return (
+                            <li key={id} className={styles.historyItem}>
+                                <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} logo`} />
+                                <div className={styles.historyItemDetails}>
+                                    <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                                    <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                    
+                                    <ul>
+                                        {historyItem.experiences.map((expereance, id) => {
+                                            return (
+                                                <li key={id}>
+                                                    {expereance}
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
 
-                })}
-            </ul>
-        </div>
+                            </li>
+                        )
 
-    </section>
-  )
+                    })}
+                </ul>
+            </div>
+
+        </section>
+    )
 }
 
 export default Expereance
