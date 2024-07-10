94c90148a5746108f4cc79d64feb58811f2dce6a
Derrick Napokoli <napokoliderrick88@gmail.com>
2024-07-10 23:29:57 +0300

    More Styles for experience component

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { getImageUrl } from '../../utils'
+import styles from '../Projects/ProjectCard.module.css'
 
 const ProjectCard = ( { project, id }) => {
   return (
-    <div >
-                        <img src={getImageUrl(project.imageSrc)} alt={`Image of ${project.title}`} />
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <ul>
+    <div className={styles.container}>
+                        <img src={getImageUrl(project.imageSrc)} 
+                        alt={`Image of ${project.title}`} className={styles.image}/>
+                        <h3 className={styles.title}>{project.title}</h3>
+                        <p className={styles.description}>{project.description}</p>
+                        <ul className={styles.skills}>
                             {project.skills.map((skill, id) => {
-                                return <li key={id}>{skill}</li>
+                                return <li key={id} className={styles.skill}>{skill}</li>
                             })}
                         </ul>
-                        <div>
-                            <a href={project.demo}>Demo</a>
-                            <a href={project.source}>Source</a>
+                        <div className={styles.links}>
+                            <a href={project.demo} className={styles.link}>Demo</a>
+                            <a href={project.source} className={styles.link}>Source</a>
                         </div>
 
                     </div>
