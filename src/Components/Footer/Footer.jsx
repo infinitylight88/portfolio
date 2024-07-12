0e666dddfdf0c0d5362e851ae19f278edd310b77
Derrick Napokoli <napokoliderrick88@gmail.com>
2024-07-12 14:58:39 +0300

    Completed Footer styling
    Footer Markup and styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import styles from './Footer.module.css'
+import { getImageUrl } from '../../utils'
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer id='contact' className={styles.container}>
+      <div className={styles.text}>
         <h2>Contact me</h2>
         <p>Feel free to get in-touch</p>
       </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="" />
+          <a href="mailto:napokoliderrick88@gmail.com">napooliderrick88@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/linkedinIcon.png")} alt=""/>
+          <a href="https://www.linkedin.com/messaging/thread/new/">Derrick Napokoli</a>
+        </li>
+        <li className={styles.link}>
+        <img src={getImageUrl("contact/githubIcon.png")} alt=""/>
+        <a href="https://github.com/infinitylight88">Derrick Napokoli </a>
+        </li>
+        
+        
+      </ul>
     </footer>
   )
 }
